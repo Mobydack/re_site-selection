927a7a13de7cfe6bc9686933ea312c2ef2be14d7
Mobydack <devzetsubou@gmail.com>
2018-01-17 17:15:52 +0300

    feature(index.html)=>(add layout html code(primary for table)//feature(Scss files)=>(added style for type view - table)//feature(main.js)=>(added code for button view and button modalView, added code for event to resize window))

--- app/script/main.js
+++ app/script/main.js
@@ -5,7 +5,8 @@
         bottomWrapper: document.querySelector('.bottom-wrapper'),
         headerWrapper: document.querySelector('.header-wrapper'),
         modalWrapper:  document.querySelector('.modal-wrapper'),
-        modal: document.querySelector('.modal')
+        modal: document.querySelector('.modal'),
+        createNewSite: document.querySelector('.tile.create-new-site')
     }
     listElements.buttonModalView.addEventListener('click', viewEvent);
     const arrCard = Array.from(document.querySelectorAll('.modal__item-site'));
@@ -13,8 +14,20 @@
     arrButtonViewCard.forEach((el, i, arr) => {
         el.addEventListener('click', toggleClass);
     })
+    document.querySelector(".close-modal").addEventListener('click', viewEvent);
+    window.addEventListener('resize', ()=> {
+        if(window.innerWidth <= 768 && arrCard[0].classList.contains('modal__item-site--table')) {
+            toggleClass();
+        }
+    });
+    // $(window).resize(function(){
+    //     if($('body').innerWidth() <= 768 && $(".tile").hasClass('tile-table')){
+    //         addClass();
+    //     }
+    // });
     function toggleClass() {
         listElements.modal.classList.toggle('modal--table');
+        listElements.createNewSite.classList.toggle('create-new-site--table');
         arrButtonViewCard.forEach((el, i, arr) => {
             el.classList.toggle('view-select--active');
         });
