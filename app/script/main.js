3b50b1e42858ee5ac6bde9e196a662ab7df1248e
Mobydack <devzetsubou@gmail.com>
2018-01-18 18:27:50 +0300

    asd

--- app/script/main.js
+++ app/script/main.js
@@ -14,17 +14,47 @@
     arrButtonViewCard.forEach((el, i, arr) => {
         el.addEventListener('click', toggleClass);
     })
+
     document.querySelector(".close-modal").addEventListener('click', viewEvent);
+
     window.addEventListener('resize', ()=> {
         if(window.innerWidth <= 768 && arrCard[0].classList.contains('modal__item-site--table')) {
             toggleClass();
         }
     });
-    // $(window).resize(function(){
-    //     if($('body').innerWidth() <= 768 && $(".tile").hasClass('tile-table')){
-    //         addClass();
-    //     }
-    // });
+
+    // $('.ukit-alt-title-img').hover(function(){
+    //     let offset = $(this).offset(),
+    //         topMargin = 20,
+    //         leftMargin = 0;
+    //     $('.ukit-alt-title-tooltip').css('opacity', '1');
+    //     $('.ukit-alt-title-tooltip').offset({top: offset.top - $('.ukit-alt-title-tooltip').height() - topMargin, left: offset.left - $(this).width() / 3 + leftMargin});
+    //   }, function(){
+    //     $('.ukit-alt-title-tooltip').css('opacity', '0');
+    //   });
+    const massAltTooltip = Array.from(document.querySelectorAll(".ukit-alt .ukit-alt-title-img"));
+    massAltTooltip.forEach((el, i, arr)=>{
+        el.addEventListener('mouseenter', hoverEnterAltTooltip);
+        el.addEventListener('mouseleave', hoverLeaveAltTooltip);
+    });
+    function hoverEnterAltTooltip() {
+        console.log(this)
+        const offset = this.getBoundingClientRect(),
+            topMargin = 20,
+            leftMargin = 20;
+        console.log(this.clientHeight);
+        const tooltip = document.querySelector(`.ukit-alt-tooltip.${this.dataset.tooltipHover}`);
+        console.log(tooltip);
+        tooltip.style.top = `${offset.top - tooltip.getBoundingClientRect().height - topMargin}px`;
+        tooltip.style.left = `${offset.left - tooltip.getBoundingClientRect().width / 2 + offset.width / 2}px`;
+        tooltip.style.opacity = 1;
+    }
+
+    function hoverLeaveAltTooltip() {
+        const tooltip = document.querySelector(`.ukit-alt-tooltip.${this.dataset.tooltipHover}`);
+        tooltip.style.opacity = 0;
+    }
+
     function toggleClass() {
         listElements.modal.classList.toggle('modal--table');
         listElements.createNewSite.classList.toggle('create-new-site--table');
@@ -35,6 +65,7 @@
             el.classList.toggle('modal__item-site--table');
         });
     }
+
     function viewEvent() {
         listElements.buttonModalView.classList.toggle('open-modal-view--active');
         listElements.bottomWrapper.classList.toggle('bottom-wrapper--active');
