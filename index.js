45d25c56aec795fb11b6572c8aebe050cd7672f0
Mobydack <devzetsubou@gmail.com>
2018-02-06 12:00:19 +0300

    test

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const mime = require('mime');
+const url = require('url');
+const execFile = require("child_process").execFile;
 
 
-http.createServer((req, res) => {
-    let pathname = path.normalize(((req.url === "/") ? `${req.url}index.html` : req.url));
-    fs.stat(pathname, (err, stats)=>{
+let PORT = process.env.PORT || 3000;
+
+const basePath = "./app"
+http.createServer((req, res)=>{
+    let urlPath = url.parse(req.url).pathname;
+    let pathName = path.normalize(basePath + ((urlPath === "/") ? "/index.html" : urlPath));
+    console.log(`urlPath: ${urlPath}\npathName: ${pathName}`);
+    fs.stat(pathName, (err, stats) => {
         if(err) {
-            res.writeHead(404);
-            res.write('Resource missing 404');
+            res.writeHead(404)
+            res.write('Error 404');
             res.end();
-        } else if(stats.isFile()) {
-            let type = mime.getType(pathname);
-            res.setHeader('Content-Type', type);
-            let file = fs.createReadStream(pathname);
-            file.on('open', () => {
-                res.statusCode = 200;
-                file.pipe(res);
-            })
-            file.on('error', (err) => {
-                res.statusCode = 403;
-                res.write('file permission');
-                res.end();
-            })
+        }
+        else if(stats.isFile()) {
+            execFile('file',['-b','--mime-type',pathName],function(error,stdout,stderr) {
+               let type =  stdout.trim();
+               console.log(type);
+                res.setHeader('Content-Type', type);
+                let file = fs.createReadStream(pathName);
+                file.on('open', ()=>{
+                    res.statusCode = 200;
+                    file.pipe(res);
+                });
+                file.on('error', (err) => {
+                    res.statusCode = 403;
+                    res.write('file permission');
+                    res.end();
+                })
+            });
         } else {
             res.writeHead(403);
             res.write('Directory access is forbidden');
             res.end();
         }
-    })
-}).listen(process.env.PORT);
+    });
+}).listen(PORT);
 
-
-console.log(`server started on ${process.env.PORT} PORT`)
+console.log(`Server running at ${PORT} port`);
