fdb1b75b55d5e8e6edaa0e7326bd8af7bd6258bb
Mobydack <devzetsubou@gmail.com>
2018-02-09 12:01:49 +0300

    GULPFILE.JS:[added support autoprefixer]

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict'
 var gulp = require('gulp');
 var sass= require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
  return gulp.src('./app/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('./app/css'));
 });
 
